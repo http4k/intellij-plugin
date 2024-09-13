9aa8fa0ce225260b5d1eb845e2f486b135249b03
David Denton <denton.david@gmail.com>
2024-09-13 18:10:20 +0100

    Refactor code for generatlity

--- src/main/kotlin/org/http4k/intellij/action/GenerateCode.kt
+++ src/main/kotlin/org/http4k/intellij/action/GenerateCode.kt
@@ -9,21 +9,22 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.wm.impl.status.StatusBarUtil
+import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.onFailure
 import dev.forkhandles.result4k.orThrow
+import org.http4k.cloudnative.RemoteRequestFailed
 import org.http4k.format.Jackson
 import org.http4k.format.JacksonYaml
 import org.http4k.intellij.utils.unzipInto
-import org.http4k.intellij.wizard.ClientApiStyle
 import org.http4k.intellij.wizard.ClientApiStyle.connect
 import org.http4k.intellij.wizard.ClientApiStyle.standard
 import org.http4k.intellij.wizard.ToolboxApi
 import java.io.File
+import java.io.InputStream
 import java.time.Clock
 import java.time.temporal.ChronoUnit.SECONDS
 
-open class GenerateOpenApiCode(private val style: ClientApiStyle) : AnAction(), ActionUpdateThreadAware {
-    private val toolbox = ToolboxApi()
+abstract class GenerateCode(private val functionName: String) : AnAction(), ActionUpdateThreadAware {
     override fun update(e: AnActionEvent) {
         val file = e.dataContext.getData(VIRTUAL_FILE)
         e.presentation.setEnabledAndVisible(file?.fileIsOpenApiSpec() ?: false)
@@ -34,15 +35,15 @@ open class GenerateOpenApiCode(private val style: ClientApiStyle) : AnAction(), 
     override fun actionPerformed(e: AnActionEvent) {
         val file = e.dataContext.getData(VIRTUAL_FILE) ?: return
         val http4kDir = File(e.project!!.basePath, ".http4k")
-        val openApiDir = File(http4kDir, "openapi")
+        val functionDir = File(http4kDir, functionName)
 
         val target = File(
-            openApiDir,
+            functionDir,
             file.name + "-generated-" +
                 Clock.systemUTC().instant().truncatedTo(SECONDS).toString().filter { it.isLetterOrDigit() }
         ).apply { mkdirs() }
 
-        toolbox.generateOpenApiClasses(style, file.inputStream)
+        file.generateCode()
             .onFailure { it.orThrow() }
             .unzipInto(target)
 
@@ -51,13 +52,22 @@ open class GenerateOpenApiCode(private val style: ClientApiStyle) : AnAction(), 
         updateStatusBar(e.project!!, target)
     }
 
+    abstract fun VirtualFile.generateCode(): Result<InputStream, RemoteRequestFailed>
+
     private fun updateStatusBar(project: Project, target: File) {
         StatusBarUtil.setStatusBarInfo(project, "OpenApi classes written to ${target.absolutePath}")
     }
 }
 
-class GenerateOpenApiCodeStandard : GenerateOpenApiCode(standard)
-class GenerateOpenApiCodeConnect : GenerateOpenApiCode(connect)
+class GenerateOpenApiCodeStandard : GenerateCode("openapi") {
+    override fun VirtualFile.generateCode() =
+        ToolboxApi().generateOpenApiClasses(standard, inputStream)
+}
+
+class GenerateOpenApiCodeConnect : GenerateCode("openapi") {
+    override fun VirtualFile.generateCode() =
+        ToolboxApi().generateOpenApiClasses(connect, inputStream)
+}
 
 private fun VirtualFile.fileIsOpenApiSpec(): Boolean {
     if (!(name.endsWith(".json") || name.endsWith(".yaml"))) return false
